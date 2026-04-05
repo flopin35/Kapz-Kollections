cfe49d70869190c637d2d208c9dd0b160dbf2839
Kapz CI <action@local>
2026-04-05 15:10:56 +0000

    Fix: Use absolute paths for all assets and fix preview iframe

--- Main web/script.js
+++ Main web/script.js
@@ -5,7 +5,7 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore, collection, doc, getDoc, onSnapshot } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-import { firebaseConfig } from "./firebase-config.js";
+import { firebaseConfig } from "/firebase-config.js";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
